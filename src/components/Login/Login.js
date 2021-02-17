520b904a9b4a64eecab9cd8c99f1b64c2f18f063
choieunji <ghk784@dgu.ac.kr>
2021-02-17 11:50:08 +0900

    로그인 페이지 구성

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import logo from 'images/logo.png';
+import { Link } from 'react-router-dom';
 import './Login.scss';
 
 class Login extends Component {
@@ -8,7 +9,35 @@ class Login extends Component {
     }
 
     render() {
-        return <div className="login"></div>;
+        return (
+            <div className="login">
+                <div className="login__logo">
+                    <img src={logo}></img>
+                </div>
+                <div className="login__input">
+                    <input></input>
+                    <input></input>
+                    <div>
+                        <select></select>
+                        <span>자동 로그인</span>
+                    </div>
+                </div>
+                <div className="login__searchandsignup">
+                    <div>
+                        <Link to="/find/id">아이디 찾기</Link>
+                    </div>
+                    <div>
+                        <Link to="/find/pw">비밀번호 찾기</Link>
+                    </div>
+                    <div>
+                        <Link to="signup">회원가입</Link>
+                    </div>
+                </div>
+                <div className="login__button">
+                    <button>로그인</button>
+                </div>
+            </div>
+        );
     }
 }
 
