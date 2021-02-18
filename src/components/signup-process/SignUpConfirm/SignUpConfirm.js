6e269c305d00454a9c4fded5916c7d5e6b3404c4
choieunji <ghk784@dgu.ac.kr>
2021-02-19 03:21:08 +0900

    SignUp 본인인증 PC버전 완료

--- src/components/signup-process/SignUpConfirm/SignUpConfirm.js
+++ src/components/signup-process/SignUpConfirm/SignUpConfirm.js
@@ -8,16 +8,24 @@ const SignUpConfirm = () => {
         <div className="signupconfirm">
             <div className="signupconfirm__title">
                 <span className="not-pc">02 </span>
-                <span>본인확인</span>
+                <span className="not-pc">본인확인</span>
+                <span className="only-pc">본인인증</span>
             </div>
             <div className="not-pc">
                 <img src={step2}></img>
             </div>
             <div className="signupconfirm__content">
-                <span>회원가입을 위하여</span>
-                <span className="signupconfirm__content tablet">고객님 명의의 휴대전화로 본인인증이 필요합니다.</span>
-                <span className="signupconfirm__content mobile">고객님 명의의 휴대전화로</span>
-                <span className="signupconfirm__content mobile">본인인증이 필요합니다.</span>
+                <div className="not-pc">
+                    <span>회원가입을 위하여</span>
+                    <span className="signupconfirm__content tablet">
+                        고객님 명의의 휴대전화로 본인인증이 필요합니다.
+                    </span>
+                    <span className="signupconfirm__content mobile">고객님 명의의 휴대전화로</span>
+                    <span className="signupconfirm__content mobile">본인인증이 필요합니다.</span>
+                </div>
+                <div className="only-pc">
+                    <button>휴대폰으로 본인확인 하기</button>
+                </div>
             </div>
         </div>
     );
