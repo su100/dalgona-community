e9e62c852f51dc09fd5803a71097ea797b855178
choieunji <ghk784@dgu.ac.kr>
2021-02-19 15:46:53 +0900

    페이지 네이션 제작

---
+++ src/components/common/Pagination/Pagination.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Pagination.scss';
+
+const Pagination = ({ countList, currentPage, handlePage, isReply }) => {
+    const pageRenderer = ({ count = countList }) => {
+        const lastPage = Math.ceil(count / 5) === 0 ? 1 : Math.ceil(count / 5); //가장 마지막 페이지, 1페이지 이하일 때는 1
+        const startPage = Math.floor((currentPage - 1) / 5) * 5 + 1; //현재 페이지가 속하는 첫 페이지
+        const endPage = Math.min(startPage + 4, lastPage); //현재 페이지가 속하는 마지막페이지
+        let result = [];
+        let tmp = [];
+        const pre = '<';
+        const next = '>';
+
+        if (lastPage === 1) {
+            return (
+                <button className="page-num current" id={currentPage} onClick={handlePage} value={1}>
+                    1
+                </button>
+            );
+        }
+
+        if (currentPage !== 1 && currentPage > 5) {
+            //첫페이지 아닐 때
+            tmp.push(
+                <button id={currentPage} key="prev" onClick={handlePage} value={currentPage - 5}>
+                    {pre}
+                </button>
+            );
+        }
+        for (let i = startPage; i <= endPage; i++) {
+            tmp.push(
+                <button
+                    className={parseInt(currentPage) === i ? 'page-num-click' : 'page-num'}
+                    key={i}
+                    onClick={handlePage}
+                    value={i}
+                    id={i}
+                >
+                    {i}
+                </button>
+            );
+        }
+        if (currentPage !== lastPage && currentPage < lastPage - 5) {
+            //마지막페이지 아닐 때
+            console.log(currentPage);
+            tmp.push(
+                <button key="next" id={currentPage} onClick={handlePage} value={parseInt(currentPage) + parseInt(5)}>
+                    {next}
+                </button>
+            );
+        }
+        result.push(<div>{tmp}</div>);
+        return result;
+    };
+    return <div className="pagination"> {pageRenderer(countList)} </div>;
+};
+
+export default Pagination;
