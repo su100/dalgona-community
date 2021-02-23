91d781adb6df2bc976bc4dccb2d5e517bcf53423
choieunji <ghk784@dgu.ac.kr>
2021-02-23 20:15:54 +0900

    ActivityList 컴포넌트 추가

---
+++ src/components/common/ActivityList/ActivityList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './ActivityList.scss';
+
+const ActivityList = (props) => {
+    return <div className="activity-list"></div>;
+};
+
+export default ActivityList;
