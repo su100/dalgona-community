88a890bd7c8111f492abb9719efdfe01df41a532
choieunji <ghk784@dgu.ac.kr>
2021-02-24 00:55:17 +0900

    Point 컴포넌트 모바일 완료

--- src/components/Point/Point.js
+++ src/components/Point/Point.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PointList from 'components/common/PointList';
+import star from 'images/star.png';
 
 import './Point.scss';
 
@@ -14,7 +15,19 @@ class Point extends Component {
             <div className="point">
                 <div className="not-pc">
                     <div className="point__header">
-                        <span>활동내역</span>
+                        <span>별내역</span>
+                    </div>
+                    <div className="point__mypoint">
+                        <span>현재 나의 별</span>
+                        <div className="point__mypoint-star">
+                            <span>+6개</span>
+                            <img src={star}></img>
+                        </div>
+                    </div>
+                    <div className="point__select">
+                        <button>충전</button>
+                        <button>사용</button>
+                        <button>회수</button>
                     </div>
                 </div>
                 <PointList />
