0fb46fbca3de454e26eb265cf58852b8cd366bdf
choieunji <ghk784@dgu.ac.kr>
2021-02-24 18:37:58 +0900

    기존 Profile 컴포넌트 EditProfile 과 Profile 로 분리

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,58 +1,45 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import profile from 'images/profile.png';
-import photoIcon from 'images/photo.png';
-
+import EditProfile from 'components/common/EditProfile';
+import ActivityList from 'components/common/ActivityList';
+import PointList from 'components/common/PointList';
 import './Profile.scss';
 
 class Profile extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            type: 'activity',
+        };
     }
 
+    handleType = (e) => {
+        this.setState({ type: e.target.id });
+    };
+
     render() {
+        const { type } = this.state;
         return (
             <div className="profile">
-                <div className="profile__header">
-                    <div className="not-pc">
-                        <span>프로필수정</span>
+                <EditProfile />
+                <div className="only-pc">
+                    <div className="profile__listtype">
+                        <button
+                            className={type === 'activity' ? 'profile__listtype click' : undefined}
+                            onClick={this.handleType}
+                            id="activity"
+                        >
+                            활동내역
+                        </button>
+                        <button
+                            className={type === 'point' ? 'profile__listtype click' : undefined}
+                            onClick={this.handleType}
+                            id="point"
+                        >
+                            별 내역
+                        </button>
                     </div>
-                </div>
-                <div className="profile__img">
-                    <input
-                        type="file"
-                        accept="image/*"
-                        ref={this.fileInput}
-                        onChange={this.selectImg}
-                        onClick={(event) => {
-                            event.target.value = null;
-                        }}
-                    />
-                    <button className="" onClick={this.onClickSelect}>
-                        <img src={photoIcon} alt="photoIcon" />
-                    </button>
-                </div>
-                <div className="profile__content">
-                    <div className="profile__content-nickname">
-                        <span>닉네임 </span>
-                        <div className="profile__content-nickname button">
-                            <input></input>
-                            <button>중복확인</button>
-                        </div>
-                    </div>
-                    <div className="profile__content-introduce">
-                        <span>자기소개</span>
-                        <input></input>
-                        <div className="only-pc">
-                            <button>수정</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="not-pc">
-                    <div className="profile__button">
-                        <button className="profile__button-modify">수정</button>
-                        <button className="profile__button-secession">탈퇴</button>
-                    </div>
+                    {type === 'activity' && <ActivityList />}
+                    {type === 'point' && <PointList />}
                 </div>
             </div>
         );
