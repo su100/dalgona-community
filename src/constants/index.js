b287e229ae82efea835e0f9d002ee3ec20120bb2
su100 <zxctys@naver.com>
2021-02-24 22:50:15 +0900

    enter test server address

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,8 +4,7 @@ let url;
 if (process.env.NODE_ENV === 'production') {
     url = 'https://dalgonatest3.cafe24.com';
 } else {
-    url = 'http://localhost:8000';
-    //url = "https://dalgonatest3.cafe24.com";
+    url = 'https://dalgona-test-aqact.run.goorm.io/';
 }
 
 export const API_BASE_URL = url;
