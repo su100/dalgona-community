1572c196758d4bcb98f82e6c771d2ea21d5112cf
choieunji <ghk784@dgu.ac.kr>
2021-03-01 11:55:38 +0900

    LunaPost Component View 로 분리 및 Write Component 추가

--- src/components/LunaPost/LunaPost.js
+++ src/components/LunaPost/LunaPost.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CommentList from 'components/common/comment/CommentList';
-import heart from 'images/heart.png';
+import View from 'components/common/View';
 import './LunaPost.scss';
 
 class LunaPost extends Component {
@@ -74,41 +74,7 @@ class LunaPost extends Component {
     render() {
         return (
             <div className="lunapost">
-                <div className="lunapost__detail">
-                    <span>
-                        홈{'>'}루나{'>'}비투비
-                    </span>
-                </div>
-                <div className="lunapost__title">
-                    <span> 제목을 써주세요.</span>
-                </div>
-                <div className="lunapost__info">
-                    <span>닉네임</span>
-                    <span>02/01 21:01</span>
-                    <span>조회수 100</span>
-                </div>
-                <div className="lunapost__content"></div>
-                <div className="lunapost__post">
-                    <span>글르르르ㅡ르르</span>
-                </div>
-                <div className="only-pc">
-                    <div className="lunapost__postcount">
-                        <div className="lunapost__postcount-recommend">
-                            <span>추천</span>
-                            <span>123</span>
-                        </div>
-                        <div className="lunapost__postcount-reply">
-                            <span>댓글</span>
-                            <span>123</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="not-pc">
-                    <div className="lunapost__postcount">
-                        <img src={heart}></img>
-                        <span>추천 6</span>
-                    </div>
-                </div>
+                <View />
                 <CommentList commentList={this.state.commentList} isRecommend />
             </div>
         );
---
+++ src/components/Write/Write.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Write.scss';
+
+class Write extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div className="write"> </div>;
+    }
+}
+
+export default Write;
