9d02014e18063bb9450c74c4351c1b445723810a
su100 <zxctys@naver.com>
2021-03-03 22:11:48 +0900

    Header: add starIcon

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import SearchInput from 'components/common/SearchInput';
+import StarIcon from 'images/star.png';
+import StarFilledIcon from 'images/star_filled.png';
 import './Header.scss';
 
-const Header = ({ title, hasWrite }) => {
+const Header = ({ title, hasWrite, isBookmarked }) => {
     return (
         <div className="header">
             <div className="header__row">
                 <div className="header__title">
                     <h4>{title}</h4>
-                    <button>☆</button>
+                    <button>
+                        <img src={isBookmarked ? StarFilledIcon : StarIcon} alt="bookmark" />
+                    </button>
                 </div>
                 {hasWrite && <button className="header__btn--write">글쓰기</button>}
             </div>
