06912a2633a2f8cd9e77aea65bdf79f62426e3bb
su100 <zxctys@naver.com>
2021-03-06 21:30:22 +0900

    Article: api 연동

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -3,11 +3,11 @@ import PageTemplate from 'components/common/PageTemplate';
 import NavContainer from 'containers/NavContainer';
 import ArticleContainer from 'containers/ArticleContainer';
 
-const ArticlePage = ({ history, location }) => {
+const ArticlePage = ({ history, match, location }) => {
     return (
         <div>
             <PageTemplate header={<NavContainer history={history} location={location} isHome={true} />}>
-                <ArticleContainer />
+                <ArticleContainer history={history} match={match} location={location} />
             </PageTemplate>
         </div>
     );
