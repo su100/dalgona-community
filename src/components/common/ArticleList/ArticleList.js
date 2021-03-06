cec2f82bf39f0a8d3d7f47445f57dd23baed90ef
su100 <zxctys@naver.com>
2021-03-06 19:26:26 +0900

    ArticleList: 이미지 없을 때 배경색으로

--- src/components/common/ArticleList/ArticleList.js
+++ src/components/common/ArticleList/ArticleList.js
@@ -3,14 +3,15 @@ import React from 'react';
 import './ArticleList.scss';
 
 const ArticleList = ({ articleList }) => {
-    console.log(articleList);
     return (
         <div className="article-list">
             {articleList.map((article, index) => {
                 return (
                     <a href={article.link} target="_blank" rel="noreferrer" key={index} className="article-list__item">
-                        <img src={article.image} alt="news" />
-                        <div>
+                        <div className="article-list__image">
+                            {article.image && <img src={article.image} alt="news" />}
+                        </div>
+                        <div className="article-list__box">
                             <h6>{article.title}</h6>
                             <p>{article.date}</p>
                         </div>
