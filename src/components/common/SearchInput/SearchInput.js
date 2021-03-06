f0465906f877efbddf8b59effa0484218f4b23fb
su100 <zxctys@naver.com>
2021-03-06 21:33:20 +0900

    SearchInput: Enter 누르면 검색

--- src/components/common/SearchInput/SearchInput.js
+++ src/components/common/SearchInput/SearchInput.js
@@ -4,6 +4,12 @@ import searchIcon from 'images/searchIcon.png';
 import './SearchInput.scss';
 
 const SearchInput = ({ searchWord, handleChange, placeholder, getSearch }) => {
+    const handleKeyPress = (e) => {
+        //Enter 누르면 검색
+        if (e.key === 'Enter') {
+            getSearch();
+        }
+    };
     return (
         <div className="search-input">
             <input
@@ -13,6 +19,7 @@ const SearchInput = ({ searchWord, handleChange, placeholder, getSearch }) => {
                 id="searchWord"
                 value={searchWord}
                 onChange={handleChange}
+                onKeyPress={handleKeyPress}
             />
             <button onClick={getSearch}>
                 <img src={searchIcon} alt="search" />
