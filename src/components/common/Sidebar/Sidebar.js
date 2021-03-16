d236bc3f343df19352d7c59f0cff63462f370abd
choieunji <ghk784@dgu.ac.kr>
2021-03-17 02:20:49 +0900

    Sidebar 디자인 변경

--- src/components/common/Sidebar/Sidebar.js
+++ src/components/common/Sidebar/Sidebar.js
@@ -8,50 +8,54 @@ const Sidebar = ({ handleSidebar, isAuthenticated, profile, signOut }) => {
     console.log(profile);
     return (
         <div className="sidebar">
-            <div className="sidebar__header">
-                <img src={logo} onClick={handleSidebar} />
+            <div className="sidebar-style">
+                <div className="sidebar-style__header">
+                    <img src={logo} onClick={handleSidebar} />
+                </div>
+                {isAuthenticated && (
+                    <div className="sidebar-style__profile">
+                        <div className="sidebar-style__profile-img">
+                            <img src={profile.get('profile_image') ? profile.get('profile_image') : avatarbox}></img>
+                        </div>
+                        <div className="sidebar-style__profile-myinformation">
+                            <span className="sidebar-style__profile-myinformation-nickname">
+                                {profile.get('nickname')}
+                            </span>
+                            <br></br>
+                            <span className="sidebar-style__profile-myinformation-description">
+                                {profile.get('introduction')}
+                            </span>
+                        </div>
+                    </div>
+                )}
+                {isAuthenticated ? (
+                    <div className="sidebar-style__link">
+                        <div className="sidebar-style__link-content">
+                            <Link to="/my/profile">프로필 수정</Link>
+                        </div>
+                        <div className="sidebar-style__link-content">
+                            <Link to="/my/activity">활동내역</Link>
+                        </div>
+                        <div className="sidebar-style__link-content">
+                            <Link to="/my/point">별내역</Link>
+                        </div>
+                        <div className="sidebar-style__link-content">
+                            <button onClick={isAuthenticated ? signOut : () => {}}>
+                                <span>로그아웃</span>
+                            </button>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="sidebar-style__link">
+                        <div className="sidebar-style__link-content">
+                            <Link to="/login">로그인</Link>
+                        </div>
+                        <div className="sidebar-style__link-content">
+                            <Link to="/signup">회원가입</Link>
+                        </div>
+                    </div>
+                )}
             </div>
-            {isAuthenticated && (
-                <div className="sidebar__profile">
-                    <div className="sidebar__profile-img">
-                        <img src={profile.get('profile_image') ? profile.get('profile_image') : avatarbox}></img>
-                    </div>
-                    <div className="sidebar__profile-myinformation">
-                        <span className="sidebar__profile-myinformation-nickname">{profile.get('nickname')}</span>
-                        <br></br>
-                        <span className="sidebar__profile-myinformation-description">
-                            {profile.get('introduction')}
-                        </span>
-                    </div>
-                </div>
-            )}
-            {isAuthenticated ? (
-                <div className="sidebar__link">
-                    <div className="sidebar__link-content">
-                        <Link to="/my/profile">프로필 수정</Link>
-                    </div>
-                    <div className="sidebar__link-content">
-                        <Link to="/my/activity">활동내역</Link>
-                    </div>
-                    <div className="sidebar__link-content">
-                        <Link to="/my/point">별내역</Link>
-                    </div>
-                    <div className="sidebar__link-content">
-                        <button onClick={isAuthenticated ? signOut : () => {}}>
-                            <span>로그아웃</span>
-                        </button>
-                    </div>
-                </div>
-            ) : (
-                <div className="sidebar__link">
-                    <div className="sidebar__link-content">
-                        <Link to="/login">로그인</Link>
-                    </div>
-                    <div className="sidebar__link-content">
-                        <Link to="/signup">회원가입</Link>
-                    </div>
-                </div>
-            )}
         </div>
     );
 };
