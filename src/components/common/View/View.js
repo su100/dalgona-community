0df55d7e0b3235daccd0f1077d59dfe4efd35db8
su100 <zxctys@naver.com>
2021-04-07 22:56:36 +0900

    View 경고 제거

--- src/components/common/View/View.js
+++ src/components/common/View/View.js
@@ -5,54 +5,52 @@ import Editor from 'components/common/Editor';
 import './View.scss';
 
 class View extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const { postInfo, type, location, isSuperuser } = this.props;
-        const path = location.pathname.split('/');
-        return (
-            <div className="view">
-                <div className="view__detail">
-                    <Link to={`/${type}/${path[2]}`}>
-                        <span>
-                            홈{'>'}
-                            {type === `luna` && `루나>${path[2]}`}
-                            {type === `free` && `자유>${path[2]}`}
-                            {type === `notice` && `달고나>공지사항`}
-                            {type === `event` && `달고나>이벤트`}
-                        </span>
-                    </Link>
-                </div>
-                <div className="view__title">
-                    <span>{postInfo.title}</span>
-                </div>
-                <div className="view__info">
-                    <div className="only-pc">
-                        <span>{postInfo.anonymous ? '익명' : postInfo.author && postInfo.author.nickname}</span>
-                        <span>{moment(new Date(postInfo.created_at)).format('YYYY/MM/DD HH:MM')}</span>
-                        <span>조회수 {postInfo.views}</span>
-                    </div>
-                    <span className="not-pc">
-                        {!postInfo.anonymous && postInfo.author ? postInfo.author.nickname : '익명 '}
-                        <span>|</span>조회수 {postInfo.views} <span>|</span>
-                        {moment(new Date(postInfo.created_at)).format('HH:MM')}
-                        <span>|</span>
-                        추천 {postInfo.recommend_count}
-                    </span>
-                    {(isSuperuser || postInfo.is_author) && (
-                        <div className="view__btn-area">
-                            <button onClick={this.props.editPost}>수정</button>
-                            <button onClick={this.props.deletePost}>삭제</button>
-                        </div>
-                    )}
-                </div>
-                <div className="view__post">
-                    <Editor readOnly contents={postInfo.body} />
-                </div>
+  render() {
+    const { postInfo, type, location, isSuperuser, editPost, deletePost } = this.props;
+    const path = location.pathname.split('/');
+    return (
+      <div className="view">
+        <div className="view__detail">
+          <Link to={`/${type}/${path[2]}`}>
+            <span>
+              홈 &lt;
+              {type === `luna` && `루나>${path[2]}`}
+              {type === `free` && `자유>${path[2]}`}
+              {type === `notice` && `달고나>공지사항`}
+              {type === `event` && `달고나>이벤트`}
+            </span>
+          </Link>
+        </div>
+        <div className="view__title">
+          <span>{postInfo.title}</span>
+        </div>
+        <div className="view__info">
+          <div className="only-pc">
+            <span>{postInfo.anonymous ? '익명' : postInfo.author && postInfo.author.nickname}</span>
+            <span>{moment(new Date(postInfo.created_at)).format('YYYY/MM/DD HH:MM')}</span>
+            <span>
+              조회수
+              {postInfo.views}
+            </span>
+          </div>
+          <span className="not-pc">
+            {!postInfo.anonymous && postInfo.author ? postInfo.author.nickname : '익명 '}
+            {` | 조회수${postInfo.views} | ${moment(new Date(postInfo.created_at)).format('HH:MM')} ${
+              postInfo.recommend_count
+            }`}
+          </span>
+          {(isSuperuser || postInfo.is_author) && (
+            <div className="view__btn-area">
+              <button onClick={editPost}>수정</button>
+              <button onClick={deletePost}>삭제</button>
             </div>
-        );
-    }
+          )}
+        </div>
+        <div className="view__post">
+          <Editor readOnly contents={postInfo.body} />
+        </div>
+      </div>
+    );
+  }
 }
 export default View;
