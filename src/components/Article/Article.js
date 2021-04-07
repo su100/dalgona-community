ae6d8fc884953c96273cb45140a1178b5fe4b2b7
ghk784@dgu.ac.kr <ghk784@dgu.ac.kr>
2021-04-07 20:17:03 +0900

    Article : eslint 적용

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -7,82 +7,85 @@ import Pagination from 'components/common/Pagination';
 import './Article.scss';
 
 class Article extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchWord: '',
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchWord: '',
+    };
+  }
+
+  handlePage = (e) => {
+    const { history, location } = this.props;
+    const query = queryString.parse(location.search);
+
+    const page = e.target.value;
+
+    // searchType:title, searchWord, page
+    if (query.search) {
+      // url에서 searchWord있는지 판별
+      history.push(`/article?page=${page}&search=${query.search}`);
+    } else {
+      history.push(`/article?page=${page}`);
     }
+  };
 
-    handlePage = (e) => {
-        const { history, location } = this.props;
-        const query = queryString.parse(location.search);
+  handleChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
 
-        const page = e.target.value;
+  searchKeyword = (e) => {
+    const word = e.target.value;
+    const { history } = this.props;
+    this.setState({ searchWord: word });
+    history.push(`/article?page=1&search=${word}`);
+  };
 
-        //searchType:title, searchWord, page
-        if (query.search) {
-            //url에서 searchWord있는지 판별
-            history.push(`/article?page=${page}&search=${query.search}`);
-        } else {
-            history.push(`/article?page=${page}`);
-        }
-    };
+  getSearch = () => {
+    // 검색하기
+    const { history } = this.props;
+    const { searchWord } = this.state;
+    history.push(`/article?page=1&search=${searchWord}`);
+  };
 
-    handleChange = (e) => {
-        this.setState({ [e.target.id]: e.target.value });
-    };
-
-    searchKeyword = (e) => {
-        const word = e.target.value;
-        this.setState({ searchWord: word });
-        this.props.history.push(`/article?page=1&search=${word}`);
-    };
-
-    getSearch = () => {
-        //검색하기
-        this.props.history.push(`/article?page=1&search=${this.state.searchWord}`);
-    };
-
-    render() {
-        const query = queryString.parse(location.search);
-        const currentPage = query.page ? Number(query.page) : 1;
-        const { newsCount, newsList, newsKeyword } = this.props;
-        return (
-            <div className="article">
-                <Header
-                    title="기사"
-                    placeholder="기사 제목을 검색하세요"
-                    searchWord={this.state.searchWord}
-                    handleChange={this.handleChange}
-                    getSearch={this.getSearch}
-                />
-                <div className="border_line" />
-                <div className="article__keyword">
-                    <Link to="/article">
-                        <h4>인기키워드</h4>
-                    </Link>
-                    {newsKeyword.map((keyword, index) => {
-                        return (
-                            <button
-                                key={index}
-                                className="article__keyword--item"
-                                value={keyword.word}
-                                onClick={this.searchKeyword}
-                            >
-                                #{keyword.word}
-                            </button>
-                        );
-                    })}
-                </div>
-                <div className="border_line" />
-                <div className="article__container">
-                    <ArticleList articleList={newsList} />
-                </div>
-                <Pagination countList={newsCount} handlePage={this.handlePage} currentPage={currentPage} />
-            </div>
-        );
-    }
+  render() {
+    const { newsCount, newsList, newsKeyword, location } = this.props;
+    const { searchWord } = this.state;
+    const query = queryString.parse(location.search);
+    const currentPage = query.page ? Number(query.page) : 1;
+    return (
+      <div className="article">
+        <Header
+          title="기사"
+          placeholder="기사 제목을 검색하세요"
+          searchWord={searchWord}
+          handleChange={this.handleChange}
+          getSearch={this.getSearch}
+        />
+        <div className="border_line" />
+        <div className="article__keyword">
+          <Link to="/article">
+            <h4>인기키워드</h4>
+          </Link>
+          {newsKeyword.map((keyword) => (
+            <button
+              key={keyword.id}
+              type="button"
+              className="article__keyword--item"
+              value={keyword.word}
+              onClick={this.searchKeyword}
+            >
+              {keyword.word}
+            </button>
+          ))}
+        </div>
+        <div className="border_line" />
+        <div className="article__container">
+          <ArticleList articleList={newsList} />
+        </div>
+        <Pagination countList={newsCount} handlePage={this.handlePage} currentPage={currentPage} />
+      </div>
+    );
+  }
 }
 
 export default Article;
