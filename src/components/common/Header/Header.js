500543865f534ee1706e4909f937d78dcffa26c2
su100 <zxctys@naver.com>
2021-04-07 22:26:43 +0900

    Header 경고 제거

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -6,46 +6,44 @@ import { Link } from 'react-router-dom';
 import './Header.scss';
 
 const Header = ({
-    boardType,
-    boardUrl,
-    title,
-    hasWrite,
-    hasBookmark,
-    isBookmarked,
-    updateBookmark,
-    placeholder,
-    searchWord,
-    getSearch,
-    handleChange,
-}) => {
-    return (
-        <div className="header">
-            <div className="header__row">
-                <div className="header__title">
-                    <h4>{title}</h4>
-                    {hasBookmark && (
-                        <button onClick={updateBookmark}>
-                            <img src={isBookmarked ? StarFilledIcon : StarIcon} alt="bookmark" />
-                        </button>
-                    )}
-                </div>
-                {hasWrite && (
-                    <button className="header__btn--write">
-                        <Link to={boardUrl ? `/${boardType}/${boardUrl}/write` : `/${boardType}/write`}>글쓰기</Link>
-                    </button>
-                )}
-            </div>
-            <hr className="not-pc" />
-            <div className="header__row">
-                <SearchInput
-                    placeholder={placeholder}
-                    searchWord={searchWord}
-                    handleChange={handleChange}
-                    getSearch={getSearch}
-                />
-            </div>
-        </div>
-    );
-};
+  boardType,
+  boardUrl,
+  title,
+  hasWrite,
+  hasBookmark,
+  isBookmarked,
+  updateBookmark,
+  placeholder,
+  searchWord,
+  getSearch,
+  handleChange,
+}) => (
+  <div className="header">
+    <div className="header__row">
+      <div className="header__title">
+        <h4>{title}</h4>
+        {hasBookmark && (
+          <button onClick={updateBookmark}>
+            <img src={isBookmarked ? StarFilledIcon : StarIcon} alt="bookmark" />
+          </button>
+        )}
+      </div>
+      {hasWrite && (
+        <button className="header__btn--write">
+          <Link to={boardUrl ? `/${boardType}/${boardUrl}/write` : `/${boardType}/write`}>글쓰기</Link>
+        </button>
+      )}
+    </div>
+    <hr className="not-pc" />
+    <div className="header__row">
+      <SearchInput
+        placeholder={placeholder}
+        searchWord={searchWord}
+        handleChange={handleChange}
+        getSearch={getSearch}
+      />
+    </div>
+  </div>
+);
 
 export default Header;
