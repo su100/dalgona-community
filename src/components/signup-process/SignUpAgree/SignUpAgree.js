28c5fabf2b73a92ee491caa3e8251a88c105d801
ghk784@dgu.ac.kr <ghk784@dgu.ac.kr>
2021-04-07 20:42:39 +0900

    signup-process : eslint 재적용

--- src/components/signup-process/SignUpAgree/SignUpAgree.js
+++ src/components/signup-process/SignUpAgree/SignUpAgree.js
@@ -5,109 +5,111 @@ import step1 from 'images/step1.png';
 import './SignUpAgree.scss';
 
 const SignUpAgree = ({ handleAgreeConfirm }) => {
-    const [agree, setAgree] = useState(false);
-    const [checkAgreeOne, setCheckAgreeOne] = useState(false);
-    const [checkAgreeTwo, setCheckAgreeTwo] = useState(false);
-    const [checkAgreeThree, setCheckAgreeThree] = useState(false);
+  const [agree, setAgree] = useState(false);
+  const [checkAgreeOne, setCheckAgreeOne] = useState(false);
+  const [checkAgreeTwo, setCheckAgreeTwo] = useState(false);
+  const [checkAgreeThree, setCheckAgreeThree] = useState(false);
 
-    const onClickAgree = () => {
-        setAgree(!agree);
-        setCheckAgreeOne(!agree);
-        setCheckAgreeTwo(!agree);
-        setCheckAgreeThree(!agree);
-        handleAgreeConfirm();
-    };
+  const onClickAgree = () => {
+    setAgree(!agree);
+    setCheckAgreeOne(!agree);
+    setCheckAgreeTwo(!agree);
+    setCheckAgreeThree(!agree);
+    handleAgreeConfirm();
+  };
 
-    const onClickCheckAgreeOne = () => {
-        setCheckAgreeOne(!checkAgreeOne);
-        if (agree || (checkAgreeTwo && checkAgreeThree)) {
-            setAgree(!agree);
-            handleAgreeConfirm();
-        }
-    };
-    const onClickCheckAgreeTwo = () => {
-        setCheckAgreeTwo(!checkAgreeTwo);
-        if (agree || (checkAgreeOne && checkAgreeThree)) {
-            setAgree(!agree);
-            handleAgreeConfirm();
-        }
-    };
-    const onClickCheckAgreeThree = () => {
-        setCheckAgreeThree(!checkAgreeThree);
-        if (agree || (checkAgreeTwo && checkAgreeOne)) {
-            setAgree(!agree);
-            handleAgreeConfirm();
-        }
-    };
+  const onClickCheckAgreeOne = () => {
+    setCheckAgreeOne(!checkAgreeOne);
+    if (agree || (checkAgreeTwo && checkAgreeThree)) {
+      setAgree(!agree);
+      handleAgreeConfirm();
+    }
+  };
+  const onClickCheckAgreeTwo = () => {
+    setCheckAgreeTwo(!checkAgreeTwo);
+    if (agree || (checkAgreeOne && checkAgreeThree)) {
+      setAgree(!agree);
+      handleAgreeConfirm();
+    }
+  };
+  const onClickCheckAgreeThree = () => {
+    setCheckAgreeThree(!checkAgreeThree);
+    if (agree || (checkAgreeTwo && checkAgreeOne)) {
+      setAgree(!agree);
+      handleAgreeConfirm();
+    }
+  };
 
-    return (
-        <div className="signupagree">
-            <div className="signupagree__title">
-                <span className="not-pc">01 </span>
-                <span>약관동의</span>
-            </div>
-            <div className="not-pc">
-                <img src={step1}></img>
-            </div>
-            <div className="signupagree__button">
-                <button className={agree ? 'click' : 'noclick'} onClick={onClickAgree}>
-                    모두 동의함(만 14세 이상)
-                </button>
-            </div>
-            <div className="signupagree__content">
-                <div className="signupagree__content-agree">
-                    <span>달고나 이용약관 동의 [필수]</span>
-                    <div className="signupagree__content-agree-check box">
-                        <input
-                            type="checkbox"
-                            id="termCheck"
-                            checked={checkAgreeOne}
-                            // src={checkAgreeOne ? clickMobileCheckBox : mobileCheckBox}
-                            onChange={onClickCheckAgreeOne}
-                        />
-                        <label htmlFor="termCheck">동의함</label>
-                    </div>
-                </div>
-                <pre className="terms-box">{termsCommunity}</pre>
-            </div>
-            <div className="signupagree__content">
-                <div className="signupagree__content-agree">
-                    <span>개인정보 수집 및 이용 동의 [필수]</span>
-                    <div className="signupagree__content-agree-check">
-                        <input
-                            type="checkbox"
-                            id="privateCheck"
-                            checked={checkAgreeTwo}
-                            // src={checkAgreeTwo ? clickMobileCheckBox : mobileCheckBox}
-                            onChange={onClickCheckAgreeTwo}
-                        />
-                        <label className="signupagree__content-agree-check box" htmlFor="privateCheck">
-                            동의함
-                        </label>
-                    </div>
-                </div>
-                <pre className="terms-box">{termsPrivate}</pre>
-            </div>
-            <div className="signupagree__content">
-                <div className="signupagree__content-agree">
-                    <span>만 14세 이상입니다. [필수]</span>
-                    <div className="signupagree__content-agree-check box">
-                        <input
-                            type="checkbox"
-                            id="ageCheck"
-                            checked={checkAgreeThree}
-                            // src={checkAgreeThree ? clickMobileCheckBox : mobileCheckBox}
-                            onChange={onClickCheckAgreeThree}
-                        />
-                        <label htmlFor="ageCheck">동의함</label>
-                    </div>
-                </div>
-                <div className="signupagree__content-impossible">
-                    만 14세 미만 고객님은 해당 서비스를 이용할 수 없습니다.
-                </div>
-            </div>
+  return (
+    <div className="signupagree">
+      <div className="signupagree__title">
+        <span className="not-pc">01 </span>
+        <span>약관동의</span>
+      </div>
+      <div className="not-pc">
+        <img src={step1} alt=" " />
+      </div>
+      <div className="signupagree__button">
+        <button type="button" className={agree ? 'click' : 'noclick'} onClick={onClickAgree}>
+          모두 동의함(만 14세 이상)
+        </button>
+      </div>
+      <div className="signupagree__content">
+        <div className="signupagree__content-agree">
+          <span>달고나 이용약관 동의 [필수]</span>
+          <div className="signupagree__content-agree-check box">
+            <label htmlFor="termCheck">
+              <input
+                type="checkbox"
+                id="termCheck"
+                checked={checkAgreeOne}
+                // src={checkAgreeOne ? clickMobileCheckBox : mobileCheckBox}
+                onChange={onClickCheckAgreeOne}
+              />
+              동의함
+            </label>
+          </div>
         </div>
-    );
+        <pre className="terms-box">{termsCommunity}</pre>
+      </div>
+      <div className="signupagree__content">
+        <div className="signupagree__content-agree">
+          <span>개인정보 수집 및 이용 동의 [필수]</span>
+          <div className="signupagree__content-agree-check">
+            <label className="signupagree__content-agree-check box" htmlFor="privateCheck">
+              <input
+                type="checkbox"
+                id="privateCheck"
+                checked={checkAgreeTwo}
+                // src={checkAgreeTwo ? clickMobileCheckBox : mobileCheckBox}
+                onChange={onClickCheckAgreeTwo}
+              />
+              동의함
+            </label>
+          </div>
+        </div>
+        <pre className="terms-box">{termsPrivate}</pre>
+      </div>
+      <div className="signupagree__content">
+        <div className="signupagree__content-agree">
+          <span>만 14세 이상입니다. [필수]</span>
+          <div className="signupagree__content-agree-check box">
+            <label htmlFor="ageCheck">
+              <input
+                type="checkbox"
+                id="ageCheck"
+                checked={checkAgreeThree}
+                // src={checkAgreeThree ? clickMobileCheckBox : mobileCheckBox}
+                onChange={onClickCheckAgreeThree}
+              />
+              동의함
+            </label>
+          </div>
+        </div>
+        <div className="signupagree__content-impossible">만 14세 미만 고객님은 해당 서비스를 이용할 수 없습니다.</div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpAgree;
--- src/components/signup-process/SignUpFinish/SignUpFinish.js
+++ src/components/signup-process/SignUpFinish/SignUpFinish.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import step4 from 'images/step4.png';
 import biglogo from 'images/biglogo.png';
 import './SignUpFinish.scss';
 
-const SignUpFinish = () => {
-    return (
-        <div className="signupfinish">
-            <div className="only-pc">
-                <div className="signupfinish__title">
-                    <span className="only-pc">가입완료!</span>
-                </div>
-                <div className="signupfinish__img">
-                    <img src={biglogo}></img>
-                </div>
-                <div className="signupfinish__description">
-                    <span>달고나에 오신걸 환영합니다!</span>
-                    <br></br>
-                    <span>루나의 세계로 당신을 초대할게요</span>
-                </div>
-                <div className="signupfinish__button">
-                    <div className="signupfinish__button button">
-                        <Link to="/">홈으로 이동</Link>
-                    </div>
-                </div>
-            </div>
-            <div className="not-pc">
-                <div className="signupfinish__title">
-                    <span>04 </span>
-                    <span>가입완료</span>
-                </div>
-                <div className="signupfinish__img">
-                    <img src={step4}></img>
-                </div>
-                <div className="signupfinish__content">
-                    <span>회원가입이 완료되었습니다.</span>
-                </div>
-            </div>
+const SignUpFinish = () => (
+  <div className="signupfinish">
+    <div className="only-pc">
+      <div className="signupfinish__title">
+        <span className="only-pc">가입완료!</span>
+      </div>
+      <div className="signupfinish__img">
+        <img src={biglogo} alt="" />
+      </div>
+      <div className="signupfinish__description">
+        <span>달고나에 오신걸 환영합니다!</span>
+        <br />
+        <span>루나의 세계로 당신을 초대할게요</span>
+      </div>
+      <div className="signupfinish__button">
+        <div className="signupfinish__button button">
+          <Link to="/">홈으로 이동</Link>
         </div>
-    );
-};
+      </div>
+    </div>
+    <div className="not-pc">
+      <div className="signupfinish__title">
+        <span>04 </span>
+        <span>가입완료</span>
+      </div>
+      <div className="signupfinish__img">
+        <img src={step4} alt="" />
+      </div>
+      <div className="signupfinish__content">
+        <span>회원가입이 완료되었습니다.</span>
+      </div>
+    </div>
+  </div>
+);
 
 export default SignUpFinish;
