90110e1aa9d86b56fce0ce926477e5196a14404e
su100 <zxctys@naver.com>
2021-04-07 22:39:36 +0900

    PointList 경고 제거

--- src/components/common/PointList/PointList.js
+++ src/components/common/PointList/PointList.js
@@ -1,41 +1,39 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import star from 'images/star_filled.png';
 import './PointList.scss';
 
-const PointList = ({ date, myPoint }) => {
-    return (
-        <div className="point-list">
-            <div className="point-list__date">{<span>{date}</span>}</div>
-            {myPoint.map((point) => {
-                return (
-                    <div key={point.id} className="point-list">
-                        <div className="point-list__activity">
-                            <div className="only-pc">
-                                <div className="point-list__activity-img"></div>
-                            </div>
-                            <div className="point-list__activity-info">
-                                <div className="point-list__activity-info-title">{point.information}</div>
-                                <div className="point-list__activity-info-luna">{point.kind}</div>
-                            </div>
-                            <div className="only-pc">
-                                <div className="point-list__activity-point">
-                                    +<img src={star}></img>
-                                    {point.points}
-                                </div>
-                            </div>
-                            <div className="not-pc">
-                                <div className="point-list__activity-point mobile">
-                                    <span className="pointcount">+ {point.points}개</span>
-                                    <img src={star}></img>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                );
-            })}
+const PointList = ({ date, myPoint }) => (
+  <div className="point-list">
+    <div className="point-list__date">
+      <span>{date}</span>
+    </div>
+    {myPoint.map((point) => (
+      <div key={point.id} className="point-list">
+        <div className="point-list__activity">
+          <div className="only-pc">
+            <div className="point-list__activity-img" />
+          </div>
+          <div className="point-list__activity-info">
+            <div className="point-list__activity-info-title">{point.information}</div>
+            <div className="point-list__activity-info-luna">{point.kind}</div>
+          </div>
+          <div className="only-pc">
+            <div className="point-list__activity-point">
+              +
+              <img src={star} alt="star" />
+              {point.points}
+            </div>
+          </div>
+          <div className="not-pc">
+            <div className="point-list__activity-point mobile">
+              <span className="pointcount">{`+${point.points}개`}</span>
+              <img src={star} alt="star" />
+            </div>
+          </div>
         </div>
-    );
-};
+      </div>
+    ))}
+  </div>
+);
 
 export default PointList;
