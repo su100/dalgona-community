49ddd0f39867bf333b449726e4a37e56ef1f53a7
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-04-07 16:31:39 +0900

    eslint: store 폴더 경고 제거

--- src/store/modules/dalgona.js
+++ src/store/modules/dalgona.js
@@ -5,13 +5,13 @@ import { pender } from 'redux-pender';
 import * as api from 'lib/api';
 
 /* 액션 타입 */
-export const LIST_NOTICE = 'dalgona/LIST_NOTICE'; //공지사항 글목록 가져오기
-export const LIST_EVENT = 'dalgona/LIST_EVENT'; //이벤트 글목록 가져오기
-export const LIST_POST = 'dalgona/LIST_POST'; //글목록 가져오기
-export const POST_INFO = 'dalgona/POST_INFO'; //글 정보 가져오기
-export const GET_POST_REPLY = 'dalgona/GET_POST_REPLY'; //댓글 리스트 가져오기
-export const GET_MY_POST = 'dalgona/GET_MY_POST'; //내가 쓴 글 조회
-export const GET_MY_POINT = 'dalgona/GET_MY_POINT'; //내 별 획득 조회
+export const LIST_NOTICE = 'dalgona/LIST_NOTICE'; //  공지사항 글목록 가져오기
+export const LIST_EVENT = 'dalgona/LIST_EVENT'; //  이벤트 글목록 가져오기
+export const LIST_POST = 'dalgona/LIST_POST'; //  글목록 가져오기
+export const POST_INFO = 'dalgona/POST_INFO'; //  글 정보 가져오기
+export const GET_POST_REPLY = 'dalgona/GET_POST_REPLY'; //  댓글 리스트 가져오기
+export const GET_MY_POST = 'dalgona/GET_MY_POST'; //  내가 쓴 글 조회
+export const GET_MY_POINT = 'dalgona/GET_MY_POINT'; //  내 별 획득 조회
 
 /* 액션 생성자 */
 export const getNoticeList = createAction(LIST_NOTICE, api.getPostList);
@@ -24,115 +24,110 @@ export const getMyPoint = createAction(GET_MY_POINT, api.getMyPoint);
 
 /* 초기 상태 정의 */
 const initialState = Map({
-    noticeCount: 0,
-    noticeList: [],
-    eventCount: 0,
-    eventList: [],
-    postInfo: [],
-    postReplyList: [],
-    postReplyCount: 0,
-    myPostCount: 0,
-    myPointCount: 0,
-    myPost: [],
-    myPoint: [],
-    myListCount: 0,
+  noticeCount: 0,
+  noticeList: [],
+  eventCount: 0,
+  eventList: [],
+  postInfo: [],
+  postReplyList: [],
+  postReplyCount: 0,
+  myPostCount: 0,
+  myPointCount: 0,
+  myPost: [],
+  myPoint: [],
+  myListCount: 0,
 });
 
 /* reducer + pender */
 export default handleActions(
-    {
-        ...pender({
-            type: LIST_NOTICE,
-            onSuccess: (state, action) => {
-                const { count, results } = action.payload.data;
-                return state.set('noticeCount', count).set('noticeList', results);
-            },
-            onFailure: (state, action) => {
-                const data = action.payload.response.data;
-                console.log(data);
-                return state;
-            },
-        }),
-        ...pender({
-            type: LIST_EVENT,
-            onSuccess: (state, action) => {
-                const { count, results } = action.payload.data;
-                return state.set('eventCount', count).set('eventList', results);
-            },
-            onFailure: (state, action) => {
-                const data = action.payload.response.data;
-                console.log(data);
-                return state;
-            },
-        }),
-        ...pender({
-            type: POST_INFO,
-            onSuccess: (state, action) => {
-                return state.set('postInfo', action.payload.data);
-            },
-            onFailure: (state, action) => {
-                const data = action.payload.response.data;
-                console.log(data);
-                return state;
-            },
-        }),
-        ...pender({
-            type: GET_POST_REPLY,
-            onSuccess: (state, action) => {
-                return state
-                    .set('postReplyList', action.payload.data.results)
-                    .set('postReplyCount', action.payload.data.count);
-            },
-            onFailure: (state, action) => {
-                const data = action.payload.response.data;
-                console.log(data);
-                return state;
-            },
-        }),
-        ...pender({
-            type: GET_MY_POST,
-            onSuccess: (state, action) => {
-                const { data } = action.payload;
-                const postList = data.results;
-                let myPostList = {};
-                for (let i = 0; i < postList.length; i++) {
-                    const date = postList[i].created_at.substring(0, 10);
-                    if (Object.keys(myPostList).includes(date)) {
-                        myPostList[date] = myPostList[date].push(postList[i]);
-                    } else {
-                        myPostList[date] = List([postList[i]]);
-                    }
-                }
-                return state.set('myPostCount', data.count).set('myPost', myPostList);
-            },
-            onFailure: (state, action) => {
-                const data = action.payload.response.data;
-                console.log(data);
-                return state;
-            },
-        }),
-        ...pender({
-            type: GET_MY_POINT,
-            onSuccess: (state, action) => {
-                const { data } = action.payload;
-                const pointList = data.results;
-                let myPointList = {};
-                for (let i = 0; i < pointList.length; i++) {
-                    const date = pointList[i].created_at.substring(0, 10);
-                    if (Object.keys(myPointList).includes(date)) {
-                        myPointList[date] = myPointList[date].push(pointList[i]);
-                    } else {
-                        myPointList[date] = List([pointList[i]]);
-                    }
-                }
-                return state.set('myPointCount', data.total).set('myListCount', data.count).set('myPoint', myPointList);
-            },
-            onFailure: (state, action) => {
-                const data = action.payload.response.data;
-                console.log(data);
-                return state;
-            },
-        }),
-    },
-    initialState
+  {
+    ...pender({
+      type: LIST_NOTICE,
+      onSuccess: (state, action) => {
+        const { count, results } = action.payload.data;
+        return state.set('noticeCount', count).set('noticeList', results);
+      },
+      onFailure: (state, action) => {
+        const { data } = action.payload.response;
+        console.log(data);
+        return state;
+      },
+    }),
+    ...pender({
+      type: LIST_EVENT,
+      onSuccess: (state, action) => {
+        const { count, results } = action.payload.data;
+        return state.set('eventCount', count).set('eventList', results);
+      },
+      onFailure: (state, action) => {
+        const { data } = action.payload.response;
+        console.log(data);
+        return state;
+      },
+    }),
+    ...pender({
+      type: POST_INFO,
+      onSuccess: (state, action) => state.set('postInfo', action.payload.data),
+      onFailure: (state, action) => {
+        const { data } = action.payload.response;
+        console.log(data);
+        return state;
+      },
+    }),
+    ...pender({
+      type: GET_POST_REPLY,
+      onSuccess: (state, action) =>
+        state.set('postReplyList', action.payload.data.results).set('postReplyCount', action.payload.data.count),
+      onFailure: (state, action) => {
+        const { data } = action.payload.response;
+        console.log(data);
+        return state;
+      },
+    }),
+    ...pender({
+      type: GET_MY_POST,
+      onSuccess: (state, action) => {
+        const { data } = action.payload;
+        const postList = data.results;
+        const myPostList = {};
+        for (let i = 0; i < postList.length; i++) {
+          const date = postList[i].created_at.substring(0, 10);
+          if (Object.keys(myPostList).includes(date)) {
+            myPostList[date] = myPostList[date].push(postList[i]);
+          } else {
+            myPostList[date] = List([postList[i]]);
+          }
+        }
+        return state.set('myPostCount', data.count).set('myPost', myPostList);
+      },
+      onFailure: (state, action) => {
+        const { data } = action.payload.response;
+        console.log(data);
+        return state;
+      },
+    }),
+    ...pender({
+      type: GET_MY_POINT,
+      onSuccess: (state, action) => {
+        const { data } = action.payload;
+        const pointList = data.results;
+        const myPointList = {};
+        for (let i = 0; i < pointList.length; i++) {
+          const date = pointList[i].created_at.substring(0, 10);
+          if (Object.keys(myPointList).includes(date)) {
+            myPointList[date] = myPointList[date].push(pointList[i]);
+          } else {
+            myPointList[date] = List([pointList[i]]);
+          }
+        }
+        return state.set('myPointCount', data.total).set('myListCount', data.count).set('myPoint', myPointList);
+      },
+      onFailure: (state, action) => {
+        const { data } = action.payload.response;
+        console.log(data);
+        return state;
+      },
+    }),
+  },
+  initialState
 );
--- src/store/modules/free.js
+++ src/store/modules/free.js
@@ -3,16 +3,15 @@ import { Map } from 'immutable';
 import { pender } from 'redux-pender';
 
 import * as api from 'lib/api';
-import { Storage } from 'lib/storage';
 
 /* 액션 타입 */
-export const VIEW_BOARD = 'free/VIEW_BOARD'; //게시판 정보 가져오기
-export const LIST_BEST = 'free/LIST_BEST'; //인기글 목록 가져오기
-export const LIST_POST = 'free/LIST_POST'; //글목록 가져오기
-export const POST_INFO = 'free/POST_INFO'; //글 정보 가져오기
-export const GET_POST_REPLY = 'free/GET_POST_REPLY'; //댓글 리스트 가져오기
-export const LIST_BOOKMARK = 'free/LIST_BOOKMARK'; //즐겨찾기 가져오기
-export const UPDATE_BOOKMARK = 'free/UPDATE_BOOKMARK'; //즐겨찾기 추가 및 삭제
+export const VIEW_BOARD = 'free/VIEW_BOARD'; //  게시판 정보 가져오기
+export const LIST_BEST = 'free/LIST_BEST'; //  인기글 목록 가져오기
+export const LIST_POST = 'free/LIST_POST'; //  글목록 가져오기
+export const POST_INFO = 'free/POST_INFO'; //  글 정보 가져오기
+export const GET_POST_REPLY = 'free/GET_POST_REPLY'; //  댓글 리스트 가져오기
+export const LIST_BOOKMARK = 'free/LIST_BOOKMARK'; //  즐겨찾기 가져오기
+export const UPDATE_BOOKMARK = 'free/UPDATE_BOOKMARK'; //  즐겨찾기 추가 및 삭제
 
 /* 액션 생성자 */
 export const getBoardInfo = createAction(VIEW_BOARD, api.getBoardInfo);
@@ -25,100 +24,87 @@ export const updateBookmark = createAction(UPDATE_BOOKMARK, api.updateBookmark);
 
 /* 초기 상태 정의 */
 const initialState = Map({
-    imageURL: '',
-    boardInfo: {},
-    bestPostList: [],
-    postCount: 0,
-    postList: [],
-    postInfo: [],
-    postReplyList: [],
-    postReplyCount: 0,
-    bookmarkList: [],
+  imageURL: '',
+  boardInfo: {},
+  bestPostList: [],
+  postCount: 0,
+  postList: [],
+  postInfo: [],
+  postReplyList: [],
+  postReplyCount: 0,
+  bookmarkList: [],
 });
 
 /* reducer + pender */
 export default handleActions(
-    {
-        ...pender({
-            type: VIEW_BOARD,
-            onSuccess: (state, action) => {
-                return state.set('boardInfo', action.payload.data);
-            },
-            onFailure: (state, action) => {
-                const data = action.payload.response.data;
-                console.log(data);
-                return state;
-            },
-        }),
-        ...pender({
-            type: LIST_BEST,
-            onSuccess: (state, action) => {
-                return state.set('bestPostList', action.payload.data);
-            },
-            onFailure: (state, action) => {
-                const data = action.payload.response.data;
-                console.log(data);
-                return state;
-            },
-        }),
-        ...pender({
-            type: LIST_POST,
-            onSuccess: (state, action) => {
-                const { count, results } = action.payload.data;
-                return state.set('postCount', count).set('postList', results);
-            },
-            onFailure: (state, action) => {
-                const data = action.payload.response.data;
-                console.log(data);
-                return state;
-            },
-        }),
-        ...pender({
-            type: LIST_BOOKMARK,
-            onSuccess: (state, action) => {
-                return state.set('bookmarkList', action.payload.data.bookmark);
-            },
-            onFailure: (state, action) => {
-                const data = action.payload.response.data;
-                console.log(data);
-                return state;
-            },
-        }),
-        ...pender({
-            type: UPDATE_BOOKMARK,
-            onSuccess: (state, action) => {
-                return state.set('bookmarkList', action.payload.data.bookmark);
-            },
-            onFailure: (state, action) => {
-                const data = action.payload.response.data;
-                console.log(data);
-                return state;
-            },
-        }),
-        ...pender({
-            type: POST_INFO,
-            onSuccess: (state, action) => {
-                return state.set('postInfo', action.payload.data);
-            },
-            onFailure: (state, action) => {
-                const data = action.payload.response.data;
-                console.log(data);
-                return state;
-            },
-        }),
-        ...pender({
-            type: GET_POST_REPLY,
-            onSuccess: (state, action) => {
-                return state
-                    .set('postReplyList', action.payload.data.results)
-                    .set('postReplyCount', action.payload.data.count);
-            },
-            onFailure: (state, action) => {
-                const data = action.payload.response.data;
-                console.log(data);
-                return state;
-            },
-        }),
-    },
-    initialState
+  {
+    ...pender({
+      type: VIEW_BOARD,
+      onSuccess: (state, action) => state.set('boardInfo', action.payload.data),
+      onFailure: (state, action) => {
+        const { data } = action.payload.response;
+        console.log(data);
+        return state;
+      },
+    }),
+    ...pender({
+      type: LIST_BEST,
+      onSuccess: (state, action) => state.set('bestPostList', action.payload.data),
+      onFailure: (state, action) => {
+        const { data } = action.payload.response;
+        console.log(data);
+        return state;
+      },
+    }),
+    ...pender({
+      type: LIST_POST,
+      onSuccess: (state, action) => {
+        const { count, results } = action.payload.data;
+        return state.set('postCount', count).set('postList', results);
+      },
+      onFailure: (state, action) => {
+        const { data } = action.payload.response;
+        console.log(data);
+        return state;
+      },
+    }),
+    ...pender({
+      type: LIST_BOOKMARK,
+      onSuccess: (state, action) => state.set('bookmarkList', action.payload.data.bookmark),
+      onFailure: (state, action) => {
+        const { data } = action.payload.response;
+        console.log(data);
+        return state;
+      },
+    }),
+    ...pender({
+      type: UPDATE_BOOKMARK,
+      onSuccess: (state, action) => state.set('bookmarkList', action.payload.data.bookmark),
+      onFailure: (state, action) => {
+        const { data } = action.payload.response;
+        console.log(data);
+        return state;
+      },
+    }),
+    ...pender({
+      type: POST_INFO,
+      onSuccess: (state, action) => state.set('postInfo', action.payload.data),
+      onFailure: (state, action) => {
+        const { data } = action.payload.response;
+        console.log(data);
+        return state;
+      },
+    }),
+    ...pender({
+      type: GET_POST_REPLY,
+      onSuccess: (state, action) =>
+        state.set('postReplyList', action.payload.data.results).set('postReplyCount', action.payload.data.count),
+      onFailure: (state, action) => {
+        const { data } = action.payload.response;
+        console.log(data);
+        return state;
+      },
+    }),
+  },
+  initialState
 );
