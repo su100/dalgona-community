ef041ec9bace1d53b0654262ad65c547598e3ec7
su100 <zxctys@naver.com>
2021-05-11 16:52:23 +0900

    회원가입- 본인인증: pc 설명 보이도록

--- src/components/signup-process/SignUpConfirm/SignUpConfirm.js
+++ src/components/signup-process/SignUpConfirm/SignUpConfirm.js
@@ -10,13 +10,12 @@ const SignUpConfirm = ({ handleCheck }) => (
       <span className="only-pc">본인인증</span>
     </div>
     <img className="signup-step" src={step2} alt="" />
-    <div className="not-pc">
-      <div className="signupconfirm__content">
-        <span>회원가입을 위하여</span>
-        <span className="signupconfirm__content tablet">고객님 명의의 휴대전화로 본인인증이 필요합니다.</span>
-        <span className="signupconfirm__content mobile">고객님 명의의 휴대전화로</span>
-        <span className="signupconfirm__content mobile">본인인증이 필요합니다.</span>
-      </div>
+    <div className="signupconfirm__content">
+      회원가입을 위하여
+      <br />
+      고객님 명의의 휴대전화로
+      <div className="br-m" />
+      본인인증이 필요합니다.
     </div>
     <button type="button" className="signupconfirm__button" onClick={handleCheck}>
       휴대폰으로 본인확인 하기
